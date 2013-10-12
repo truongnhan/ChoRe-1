d91ab140e5d0576c11a3d44587d1570c968f7ce3
truongnhan <truongnhan1176211@gmail.com>
2013-10-12 13:37:32 -0700

    Thêm FormPost SẢN phẩm và một số list nhưng chưa chi tiết

---
+++ ChoReApp/Goods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoReApp
+{
+    public  class Goods
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+        public string link { get; set; }
+    }
+}
---
+++ ChoReApp/ViewModal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoReApp
+{
+   public class ViewModal
+    {
+        public ObservableCollection<Goods> item = new ObservableCollection<Goods>();
+        public ViewModal()
+        {
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+            item.Add(new Goods() { name = "Lap Top Asus1233", price = "50000" });
+        }
+    }
+}
